c02c52333c3ed2261244f9d77a3e7a72f5c82cc9
guanaguana <saframat@gmail.com>
2022-05-18 13:55:12 +0200

    first commit

---
+++ hava.js
@@ -0,0 +1,15 @@
+//komentář
+console.log("text v konzoli 2");
+
+ for (let i = 0; i < 3 ; i++){
+        console.log(i+1 + " - tady i dosahne");  
+ }
+        console.log("tady let i nedosahne");
+        console.log("---");
+
+do{
+    let tohle="mudraptor gaming";
+    var toto="mudraptor ungaming";
+    break;
+}while(true);
+console.log("let tady nedosahne\n" + "var tady dosahne - " + toto);
