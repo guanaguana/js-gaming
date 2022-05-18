11addc4b182e215d8a6c503ae50e92d910595412
guanaguana <saframat@gmail.com>
2022-05-18 17:08:34 +0200

    pridano nahodne cislo

--- modulo/main.js
+++ modulo/main.js
@@ -1,6 +1,7 @@
 const exportovanafunkce = require("./funkce.js");
 const tostejne = require("./funkce2.js");
-const cislo=6;
+const cislo=Math.round(Math.random()*10);
+console.log("Cislo je " + cislo + "\n");
 
 console.log("funkce - cislo/2 = " + exportovanafunkce.funkce(cislo));
-console.log("funkce - cislo/2 = " + tostejne(cislo));
+console.log("funkce2 - cislo/2 = " + tostejne(cislo));
