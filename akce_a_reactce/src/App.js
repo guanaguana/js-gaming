e0ca54689b03b7e1ab90bef08c3bd08c06d2590e
guanaguana <saframat@gmail.com>
2022-05-24 11:51:36 +0200

    react

---
+++ akce_a_reactce/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let x="lizar";
+  return (
+    <div className="App">
+     <h1>hello world</h1>
+     <p>{x}</p>
+    </div>
+  );
+}
+
+export default App;
